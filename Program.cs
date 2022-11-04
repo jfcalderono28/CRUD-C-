1bbac766538eb6bb1e71e30b995f4cdafa89e0b3
jfcalderono28 <juanfelipecalderono@gmail.com>
2022-11-04 01:01:53 -0500

    CRUD C#

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+public class BdComun
+{
+   
+    public static void Main(string[] args)
+    {
+        try
+        {
+            MySqlConnectionStringBuilder builder = new();
+            builder.Server = "localhost";
+            builder.Database = "mydb";
+            builder.UserID = "root";
+            builder.Password = "";
+
+            Console.WriteLine("Conectado");
+
+
+            using var connection = new MySqlConnection(builder.ToString());
+            connection.Open();
+
+            using var command = new MySqlCommand("SELECT `apellido` FROM `emple`");
+            using var reader = command.ExecuteReader(); 
+          
+            Console.WriteLine(reader.ToString());
+
+
+
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        
+        
+    }
+   
+    
+}
+
+
